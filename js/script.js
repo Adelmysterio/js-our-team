10cdf9e81c0e12f2c4e17cb15a0ee22c0af1aab9
Adelmysterio <storliniadelmo@gmail.com>
2024-05-22 14:58:56 +0200

    completato bonus-1

--- js/script.js
+++ js/script.js
@@ -36,15 +36,26 @@ const team = [
     },
 ];
 
+
 const outputElement = document.querySelector('#output')
 
 for (i = 0; i < team.length; i++) {
-    console.log('Member '+ (i+1));
+    /*  console.log('Member '+ (i+1)); */
+    const articleElement = document.createElement('article')
+    const h2Element = document.createElement('h2')
     const pElement = document.createElement('p');
-    outputElement.appendChild(pElement);
-    for (let key in team[i]){
-        console.log(key, (team[i])[key]);
-        pElement.append((team[i])[key]);
-    };
+    const imgElement = document.createElement('img');
+    outputElement.appendChild(articleElement);
+    articleElement.appendChild(imgElement);
+    articleElement.appendChild(h2Element);
+    articleElement.appendChild(pElement);
+    h2Element.append(team[i].name);
+    pElement.append(team[i].role);
+    imgElement.src = "img/" + team[i].pic;
+
+    /*   for (let key in team[i]){
+          console.log(key, (team[i])[key]);
+          pElement.append((team[i])[key]);
+      }; */
 };
 
